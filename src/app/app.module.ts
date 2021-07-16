efe183e18edf78342cec86b15706d190ad20da39
Salman Sanjeetwala <salman405f@gmail.com>
2021-07-17 00:15:01 +0530

    sign up implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { CurrentTrainingComponent } from './training/current-training/current-tr
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingComponent } from './training/past-training/past-training.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,9 +27,9 @@ import { WelcomeComponent } from './welcome/welcome.component';
     WelcomeComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,FormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule,MaterialModule
+    BrowserAnimationsModule,MaterialModule, FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
